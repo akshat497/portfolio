108aba5ddd9b9f0db5f7b90e66f585b1563ae4b1
Akshat saini <akshatsaini@Akshats-MacBook-Air.local>
2025-12-01 16:39:37 +0530

    program

---
+++ main.js
@@ -0,0 +1,8 @@
+let number=prompt("enter the length of array")
+let newArr=[]
+for(let i=0;i<=number;i++){
+
+    let arrayValue=prompt("enter array value ",i)
+    newArr.push(arrayValue)
+
+}
---
+++ main.py
@@ -0,0 +1,108 @@
+
+
+# ============================================
+#   DICTIONARY TEACHING TOOL FOR STUDENTS
+# ============================================
+
+def show_menu():
+    print("\n======= DICTIONARY LEARNING TOOL =======")
+    print("1. Create a dictionary")
+    print("2. Access a value")
+    print("3. Add a new key-value pair")
+    print("4. Update a value")
+    print("5. Delete a key")
+    print("6. Show all keys")
+    print("7. Show all values")
+    print("8. Show all items")
+    print("9. Use get() function")
+    print("10. Use pop() function")
+    print("11. Use popitem() function")
+    print("12. Use clear() function")
+    print("13. CONVERT STRING IN LOWER CASE")
+    print("14. CONVERT STRING IN UPPER CASE")
+    print("==========================================")
+
+def dictionary_tool():
+    student_dict = {}  # empty dict
+    print("\nA fresh dictionary has been created:", student_dict)
+
+    while True:
+        show_menu()
+        choice = input("Choose an option (1-13): ")
+
+        if choice == '1':
+            key = input("Enter key: ")
+            value = input("Enter value: ")
+            student_dict[key] = value
+            print("Dictionary created/updated:", student_dict)
+
+        elif choice == '2':
+            key = input("Enter key to access: ")
+            print("Value:", student_dict.get(key, "Key not found"))
+
+        elif choice == '3':
+            key = input("Enter new key: ")
+            value = input("Enter value: ")
+            student_dict[key] = value
+            print("Updated dictionary:", student_dict)
+
+        elif choice == '4':
+            key = input("Enter key to update: ")
+            if key in student_dict:
+                value = input("Enter new value: ")
+                student_dict[key] = value
+                print("Updated dictionary:", student_dict)
+            else:
+                print("Key not found!")
+
+        elif choice == '5':
+            key = input("Enter key to delete: ")
+            if key in student_dict:
+                del student_dict[key]
+                print("Key deleted. Dictionary:", student_dict)
+            else:
+                print("Key not found!")
+
+        elif choice == '6':
+            print("All keys:", list(student_dict.keys()))
+
+        elif choice == '7':
+            print("All values:", list(student_dict.values()))
+
+        elif choice == '8':
+            print("All items (key-value pairs):", list(student_dict.items()))
+
+        elif choice == '9':
+            key = input("Enter key for get(): ")
+            print("Result:", student_dict.get(key, "Default: Key not found"))
+
+        elif choice == '10':
+            key = input("Enter key for pop(): ")
+            print("Popped value:", student_dict.pop(key, "Key not found"))
+            print("Dictionary:", student_dict)
+
+        elif choice == '11':
+            if student_dict:
+                print("Popped last item:", student_dict.popitem())
+                print("Dictionary:", student_dict)
+            else:
+                print("Dictionary is empty!")
+
+        elif choice == '12':
+            student_dict.clear()
+            print("Dictionary cleared:", student_dict)
+
+        elif choice == '13':
+            name=input("enter ur name here in upper case")
+            print(name.lower())
+              
+        elif choice == '14':
+            name=input("enter ur name here in lower case")
+            print(name.upper()) 
+            
+
+        else:
+            print("Invalid choice. Try again.")
+
+# Run the tool
+dictionary_tool()
